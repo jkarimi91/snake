14113d9cec39db5e8ff863ab85e828468f2b3032
Jay Karimi <jkarimi91@gmail.com>
2018-03-05 22:58:43 -0800

    Adding food

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 from vector import Vector
 from snake import Snake
+import random
+import itertools
 
 
 class Game(object):
@@ -16,17 +18,22 @@ class Game(object):
 
     background_color = (0, 0, 0)  # black
     snake_color = (255, 255, 255)  # white
+    food_color = (255, 0, 0)  # red
 
     def __init__(self):
         grid_center = Vector((0.5, 0.5)) * Game.grid_size
         self.snake = Snake(grid_center, Game.direction_to_vector['down'])
         self.screen = pygame.display.set_mode(Vector((1, 1)) * (Game.grid_size * Game.block_size))
+        self.food = self.get_random_coords()
 
     def out_of_bounds(self):
         x, y = self.snake.head()
         if x < 0 or x >= Game.grid_size or y < 0 or y >= Game.grid_size:
             return True
         return False
+
+    def grid_full(self):
+        return len(self.snake.body) >= Game.grid_size**2
 
     def move(self, direction):
         """
@@ -35,17 +42,33 @@ class Game(object):
         direction : {'left', 'right', 'up', 'down'}
             The direction the snake should move in.
         """
-        if not self.out_of_bounds() and not self.snake.self_intersecting():
+        if not self.out_of_bounds() and not self.snake.self_intersecting() and not self.grid_full():
             if direction in Game.direction_to_vector:
                 direction = Game.direction_to_vector[direction]
             else:
                 direction = self.snake.direction
-            self.snake.move(direction)
+
+            ate_food = self.snake.head() == self.food
+            self.snake.move(direction, grow=ate_food)
+
+            if ate_food:
+                self.food = self.get_random_coords()
+
+    @staticmethod
+    def block(grid_coord):
+        return  pygame.rect.Rect(grid_coord * Game.block_size, Vector((1, 1)) * Game.block_size)
 
     def draw(self):
         self.screen.fill(Game.background_color)
+        pygame.draw.rect(self.screen, Game.food_color, self.block(self.food))
         for p in self.snake:
-            rect = pygame.rect.Rect(p * Game.block_size, Vector((1, 1)) * Game.block_size)
-            pygame.draw.rect(self.screen, Game.snake_color, rect)
+            pygame.draw.rect(self.screen, Game.snake_color, self.block(p))
         pygame.display.update()
 
+    def get_random_coords(self):
+        possible_vals = set(xrange(Game.grid_size))
+        possible_x = possible_vals - set([coord[0] for coord in self.snake.body])
+        possible_y = possible_vals - set([coord[1] for coord in self.snake.body])
+        x = random.choice(list(possible_x))
+        y = random.choice(list(possible_y))
+        return Vector((x, y))
--- snake.py
+++ snake.py
@@ -5,7 +5,6 @@ from vector import Vector
 
 class Snake(object):
     def __init__(self, position, direction, init_len=3):
-        self.grow = False
         self.direction = direction
         self.body = deque([position - i * self.direction for i in xrange(init_len)])
 
@@ -15,25 +14,22 @@ class Snake(object):
     def pop_tail(self):
         self.body.pop()
 
-    def move(self, direction):
+    def move(self, direction, grow):
         """
         Parameters
         ----------
         direction : vector
             The direction the snake should move in.
+        grow : bool
+            Whether the snake should grow 1 segment longer.
         """
         if direction != self.direction and direction != -self.direction:
             self.direction = direction
         new_head = self.head() + self.direction
         self.body.appendleft(new_head)
 
-        if not self.grow:
+        if not grow:
             self.pop_tail()
-        else:
-            self.grow = False
-
-    def grow(self):
-        self.grow = True
 
     def self_intersecting(self):
         body = iter(self)
